ec981c7453d71e0182ccf8aede74496bbda3bc90
HSIEH, Li-Yi (SHIE, Li-Yi) <lyshie@mx.nthu.edu.tw>
2018-03-16 16:03:55 +0800

    Create upper_case.js

---
+++ tampermonkey/upper_case.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Upper Case
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Upper Case
+// @author       Hsieh Li-Yi
+// @match        https://read.moe.edu.tw/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function ($, undefined) {
+    $(function () {
+        'use strict';
+
+        function toTitleCase(str)
+        {
+            return str.replace(/(?:^|\w)\S*/g, function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+        }
+        // statically created
+        $('input:text').on('change', function() {
+            var line = toTitleCase($(this).val());
+            $(this).val(line);
+        });
+    });
+})(window.jQuery.noConflict(true));
