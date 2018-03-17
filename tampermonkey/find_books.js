69c9148365ca287ccecd645b69dd8ec82fed5d56
HSIEH, Li-Yi (SHIE, Li-Yi) <lyshie@mx.nthu.edu.tw>
2018-03-17 14:51:33 +0800

    Create find_books.js

---
+++ tampermonkey/find_books.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Find books
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Find books
+// @author       Hsieh Li-Yi
+// @match        *://read.moe.edu.tw/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+(function ($, undefined) {
+    $(function () {
+        'use strict';
+
+        function toTitleCase(str)
+        {
+            return str.replace(/(?:^|\w)\S*/g, function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+        }
+
+        $('input#name_book').on('change', function() {
+            var line = toTitleCase($(this).val());
+            $(this).val(line);
+        });
+
+        $('input#auth_content').on('change', function() {
+            var line = toTitleCase($(this).val());
+            $(this).val(line);
+        });
+
+        $('input#isbn').on('change', function() {
+            var isbn = $(this).val();
+            $.getJSON('http://www.chps.tn.edu.tw/python/isbn.py?callback=?&isbn=' + isbn, function(json) {
+                if (json) {
+                    console.log(json);
+                    $('input#name_book').val(json.Title);
+                    $('input#auth_content').val(json.Authors);
+                    if (json.Language == 'eng') {
+                        $('select#lang').val('eng');
+                    }
+                }
+            });
+        });
+    });
+})(window.jQuery.noConflict(true));
